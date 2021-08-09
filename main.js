cba769ea771cc88bfcba8980f9ed1ef89a2fec40
Gonzalo <gomzape123@gmail.com>
2021-08-09 06:39:03 -0500

    primerCommit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+
+/*carrousel bonito*/
+document.addEventListener('DOMContentLoaded', () =>{
+    const elementosCarousel= document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel,{
+        duration: 700,
+        dist: 0,
+        shift: 5,   
+        padding: 5,
+        numVisible: 5,
+		
+        indicators: true,
+       
+
+    });
+
+});
+
+/*carrousel bonito*/
+/*document.addEventListener('DOMContentLoaded', () =>{
+    const elementosCarousel= document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel,{
+        duration: 1500,
+        dist: -80,
+        shift: 5,   
+        padding: 5,
+        numVisible: 3,
+        indicators: true,
+        noWrap: true,
+
+    });
+
+});*/
